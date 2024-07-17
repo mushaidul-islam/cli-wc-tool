88a7db81046f8bf36b47e7d0c2f54ead20c5cdc2
Mushaidul Islam <123islammahin@gmail.com>
2024-07-17 15:55:54 +0600

    feat(tool): added line , words and character count ability

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
 func main() {
-	// Define a flag for the -c option
-	countBytes := flag.Bool("c", false, "count bytes")
+	// Define the flags
+	countBytes := flag.Bool("b", false, "count bytes")
+	countLines := flag.Bool("l", false, "count lines")
+	countWords := flag.Bool("w", false, "count words")
+	countChars := flag.Bool("c", false, "count characters")
 	flag.Parse()
 
-	// Check if the countBytes flag is provided
-	if !*countBytes {
-		fmt.Println("Usage: ccwc -c <filename>")
-		return
-	}
+	defaultFlag := !*countBytes && !*countLines && !*countWords && !*countChars
 
 	// Get the file name from the arguments
 	args := flag.Args()
@@ -25,16 +27,62 @@ func main() {
 	}
 	fileName := args[0]
 
-	// Read the file
-	fileData, err := os.ReadFile(fileName)
+	// Open the file
+	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Printf("Error reading file %s: %v\n", fileName, err)
 		return
 	}
+	defer file.Close()
 
-	// Get the number of bytes
-	numBytes := len(fileData)
+	scanner := bufio.NewScanner(file)
 
-	// Print the result
-	fmt.Printf("%d %s\n", numBytes, fileName)
+	var bytesCount, lineCount, wordCount, charCount int
+
+	getLineCount := defaultFlag || *countLines
+	getBytesCount := defaultFlag || *countBytes
+	getWordsCount := defaultFlag || *countWords
+	getCharsCount := defaultFlag || *countChars
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if getLineCount {
+			lineCount++
+		}
+
+		if getBytesCount {
+			bytesCount += len(line) + 1 // To handle new-line character
+		}
+
+		if getWordsCount {
+			wordCount += len(strings.Fields(line))
+		}
+
+		if getCharsCount {
+			charCount += utf8.RuneCountInString(line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	var result []string
+
+	if getBytesCount {
+		result = append(result, fmt.Sprintf("bytes: %d", bytesCount))
+	}
+	if getLineCount {
+		result = append(result, fmt.Sprintf("lines: %d", lineCount))
+	}
+	if getWordsCount {
+		result = append(result, fmt.Sprintf("words: %d", wordCount))
+	}
+	if getCharsCount {
+		result = append(result, fmt.Sprintf("characters: %d", charCount))
+	}
+
+	fmt.Print(strings.Join(result, ", "))
 }
